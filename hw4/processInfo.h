b3cb43c2ae6231d76e24234ec124c25d5c9a79bf
yarinedri1 <Yarinedri116@gmail.com>
2025-12-31 22:12:42 +0200

    proc.c and proc.h

--- hw4/processInfo.h
+++ hw4/processInfo.h
@@ -1,1 +1,12 @@
+#ifndef PROCESSINFO_H
+#define PROCESSINFO_H
 
+struct processInfo {
+    int state;      // process state
+    int ppid;       // parent PID
+    int sz;         // size of process memory, in bytes
+    int nfd;        // number of open file descriptors
+    int nrswitch;   // number context switches in
+};
+
+#endif
